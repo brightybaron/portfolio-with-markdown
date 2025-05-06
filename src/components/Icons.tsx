6e74afdb6435f12b9fd806c40b3eb95065477454
brightybaron <ardi.panda54@protonmail.com>
2025-05-06 14:39:47 +0700

    edit edit

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -209,6 +209,26 @@ export const IconGraduationCap = () => (
   </svg>
 );
 
+export const IconChat = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+    <path d="M8 12h.01" />
+    <path d="M12 12h.01" />
+    <path d="M16 12h.01" />
+  </svg>
+);
+
 export const Icons = {
   IconHome,
   IconPeople,
@@ -222,6 +242,7 @@ export const Icons = {
   IconPencilRuler,
   IconLaptop,
   IconGraduationCap,
+  IconChat,
 };
 
 export default Icons;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,24 @@ export const formatDate = (dateStr: string, path?: string) => {
 
   return formatter.format(date);
 };
+
+/**
+ * Format an Indonesian phone number:
+ * - Replaces the country code "62" with "0"
+ * - Groups digits in sets of 4, separated by spaces
+ *
+ * Example:
+ *   Input: 6285226822944
+ *   Output: "0852 2682 2944"
+ */
+
+export const formatPhoneNumber = (input: string | number): string => {
+  let str = input.toString();
+
+  if (str.startsWith("62")) {
+    str = "0" + str.slice(2);
+  }
+
+  const chunks = str.match(/.{1,4}/g);
+  return chunks ? chunks.join(" ") : str;
+};
